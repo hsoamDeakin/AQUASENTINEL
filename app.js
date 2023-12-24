00a6308daab5e674430a21b8b0589d2917e4f943
Hosam <s213067525@deakin.edu.au>
2023-12-24 12:13:30 +1100

    add layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const expressLayouts = require('express-ejs-layouts')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -18,6 +19,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// Set Templating Engine
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+// Set the default layout to use for all routes
+app.set('layout extractScripts', true);
+
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -25,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
