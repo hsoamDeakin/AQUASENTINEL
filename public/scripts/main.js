bfd6bece25ce5d9f41c148efc429d6b8b49fade4
Jensen-T <jensen.tang@gmail.com>
2024-01-22 17:50:52 +1100

    changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -28,3 +28,14 @@ $(function () {
   });
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
